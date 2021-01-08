fb342046b1d31a22823d641f2d4fe26b99fccdd7
Miles Frain <miles.frain@colorado.edu>
2021-01-07 23:02:50 -0800

    Start with main, add some comments

--- test/Main.purs
+++ test/Main.purs
@@ -12,12 +12,46 @@ import Performance.Minibench (bench)
 import Random.LCG (mkSeed)
 import Test.QuickCheck.Gen (Gen, evalGen, shuffle)
 
+main :: Effect Unit
+main = do
+  let
+    genState = {newSeed: mkSeed 0, size: 1}
+    arr1 = evalGen (arrayGen 0 100 [1,0,5,5]) genState
+    arr2 = evalGen (arrayGen 20 10 [1,2,0]) genState
+
+    onlyEven x = if x `mod` 2 == 0 then Just x else Nothing
+
+  logShow arr1
+  logShow arr2
+  bench1 "mapMaybe" (mapMaybe onlyEven) arr1
+  bench1 "mapMaybe" (mapMaybe onlyEven) arr2
+  bench1 "nubEq" nubEq arr1
+  bench1 "nubEq" nubEq arr2
+  bench2 "union" union arr1 arr2
+  bench2 "union" union arr2 arr1
+  bench2 "intersect" intersect arr1 arr2
+  bench2 "intersect" intersect arr2 arr1
+  bench2 "difference" difference arr1 arr2
+  bench2 "difference" difference arr2 arr1
+
+-- | Benchmarking wrapper for a function that takes a single array
+bench1 :: forall a b. String -> (Array a -> b) -> Array a -> Effect Unit
+bench1 label func arr = do
+  log $ label <> " (" <> show (length arr) <> ")"
+  bench \_ -> func arr
+
+-- | Benchmarking wrapper for a function that takes two arrays
+bench2 :: forall a b. String -> (Array a -> Array a -> b) -> Array a -> Array a -> Effect Unit
+bench2 label func arr1 arr2 = do
+  log $ label <> " (" <> show (length arr1) <> ", " <> show (length arr2) <> ")"
+  bench \_ -> func arr1 arr2
+
 -- | Integer division where the result is rounded up.
 -- | Intended for unsigned values
 divUp :: Int -> Int -> Int
 divUp n d = (n + d - 1) / d
 
--- | Generrates an array of a desired size filled with
+-- | Generates an array of a desired size filled with
 -- | duplicates or skipped values based on provided pattern.
 -- |
 -- | Example:
@@ -25,7 +59,7 @@ divUp n d = (n + d - 1) / d
 -- |
 -- | Initial values based on desired start:
 -- | [5,6,  7  ,8,9,   10   ,11,12,  13   ]
--- | Duplicate counts created by repating provided pattern:
+-- | Duplicate counts created by repeating provided pattern:
 -- | [1,0,  3  ,1,0,    3   ,1 ,0,    3   ]
 -- | Initial values duplicated or dropped by pattern:
 -- | [5,  7,7,7,8,  10,10,10,11,  13,13,13]
@@ -49,36 +83,3 @@ arrayGen start total pattern = do
     $ take total
     $ concat
     $ zipWith replicate fullPattern shuffleInitial
-
-main :: Effect Unit
-main = do
-  let
-    genState = {newSeed: mkSeed 0, size: 1}
-    arr1 = evalGen (arrayGen 0 100 [1,0,5,5]) genState
-    arr2 = evalGen (arrayGen 20 10 [1,2,0]) genState
-
-    onlyEven x = if x `mod` 2 == 0 then Just x else Nothing
-
-  logShow arr1
-  logShow arr2
-  bench1 "mapMaybe" (mapMaybe onlyEven) arr1
-  bench1 "mapMaybe" (mapMaybe onlyEven) arr2
-  bench1 "nubEq" nubEq arr1
-  bench1 "nubEq" nubEq arr2
-  bench2 "union" union arr1 arr2
-  bench2 "union" union arr2 arr1
-  bench2 "intersect" intersect arr1 arr2
-  bench2 "intersect" intersect arr2 arr1
-  bench2 "difference" difference arr1 arr2
-  bench2 "difference" difference arr2 arr1
-
-
-bench1 :: forall a b. String -> (Array a -> b) -> Array a -> Effect Unit
-bench1 label func arr = do
-  log $ label <> " (" <> show (length arr) <> ")"
-  bench \_ -> func arr
-
-bench2 :: forall a b. String -> (Array a -> Array a -> b) -> Array a -> Array a -> Effect Unit
-bench2 label func arr1 arr2 = do
-  log $ label <> " (" <> show (length arr1) <> ", " <> show (length arr2) <> ")"
-  bench \_ -> func arr1 arr2
